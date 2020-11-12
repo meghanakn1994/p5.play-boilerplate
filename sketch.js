cc439ce77b7d70ad8a508e7e453676f84a562d87
meghanakn1994 <73427758+meghanakn1994@users.noreply.github.com>
2020-11-12 20:33:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,25 @@
-function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
-}
+const Engine=Matter.Engine
+const World=Matter.World
+const Bodies=Matter.Bodies
 
-function draw() {
-  background(255,255,255);  
-  drawSprites();
+var engine,world;
+var object
+function setup(){
+  var canvas=createCanvas(400,400);
+  engine=Engine.create();
+  world=engine.world;
+  var object_options={
+    inStatic: true
+  }
+  object=Bodies.rectangle(200,100,50,50,object_options);
+  World.add(world,object);
+  console.log(object)
+  console.log(object.type)
+  console.log(object.position.x)
+  console.log(object.position.y)
+}
+function draw(){
+  background(0);
+  rectMode(CENTER)
+  rect(object.position.x,object.position.y,50,50);
 }
